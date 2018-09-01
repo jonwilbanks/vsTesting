c4bd7ef638b305d6e6ef9984e53e2cb82d7a2937
jonwilbanks <jonwilbanks@gmail.com>
2018-08-31 23:21:39 -0500

    First

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        lblTest.Content = "You Clicked Me!"
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        lblTest.Content = ""
+    End Sub
+End Class
